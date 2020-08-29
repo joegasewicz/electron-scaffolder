e5863f7ee1b5d82f7148f20150a0ddec8106c43a
joegasewicz <joegasewicz@gmail.com>
2020-08-29 18:15:29 +0100

    help

--- main.c
+++ main.c
@@ -1,21 +1,48 @@
+/**
+ *  ELECTRON SCAFFOLDER by Joe Gasewicz
+ *  
+ *  Electron with Typescript & React starter generator
+ * 
+ *  Contact joegasewicz@gmail.com
+ **/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#define ELECTRON_SCAFFOLDER_CMD_HELP " --help"
-#define ELECTRON_SCAFFOLDER_CMD__SHORT_HELP " -h"
-#define ELECTRON_SCAFFOLDER_CMD_FLAT " --flat"
-#define ELECTRON_SCAFFOLDER_CMD_SHORT_FLAT " -f"
+#define ES_CMD_HELP "--help"
+#define ES_CMD_SHORT_HELP "-h"
+#define ES_CMD_FLAT "--flat"
+#define ES_CMD_SHORT_FLAT "-f"
+#define ES_CMD_FORM "--form"
+#define ES_ARGS_MAX_LENGTH 2
 
 typedef struct
 {
+    char *args[ES_ARGS_MAX_LENGTH];
+    int args_length;
+    char *project_name;
     char *relative_path;
 } ELECTRON_SCAFFOLDER_obj;
 
 ELECTRON_SCAFFOLDER_obj *ELECTRON_SCAFFOLDER_create(char *argv[])
 {
+    int count = 0;
     ELECTRON_SCAFFOLDER_obj *es_obj = malloc(sizeof(ELECTRON_SCAFFOLDER_obj));
     es_obj->relative_path = argv[0];
+    es_obj->project_name = argv[1];
+    es_obj->args_length = 0;
+    if(argv[2])
+    {
+        for(int i = 2; i < ES_ARGS_MAX_LENGTH; i++)
+        {
+            if(argv[i])
+            {
+                es_obj->args[count] = argv[i];
+                count++;
+                es_obj->args_length = count;
+            }
+        }
+    }
     return es_obj;
 }
 
@@ -30,9 +57,9 @@ void ELECTRON_SCAFFOLDER_help(void)
     printf("Usage: electron-scaffolder <project_name> [options]\n\n");
     printf("Options:\n");
     printf("\t%s, %s\t\tbuild the project in the current directory\n",
-        ELECTRON_SCAFFOLDER_CMD_HELP, ELECTRON_SCAFFOLDER_CMD__SHORT_HELP);
+        ES_CMD_HELP, ES_CMD_SHORT_HELP);
     printf("\t%s, %s\t\toutput usage information\n",
-    ELECTRON_SCAFFOLDER_CMD_FLAT, ELECTRON_SCAFFOLDER_CMD_SHORT_FLAT);
+    ES_CMD_FLAT, ES_CMD_SHORT_FLAT);
     printf("\t  Only <project_name> is requried.\n");
     printf("\033[0m");
 }
@@ -44,15 +71,26 @@ int main(int argc, char *argv[])
     {
         printf("ERROR: Please specify the project directory\n");
         printf("\033[1;34m\telectron-scaffold <project_name>\033[0m\n");
-        return 0;
+        return EXIT_FAILURE;
     }
-    if(strcmp(argv[1], ELECTRON_SCAFFOLDER_CMD_HELP))
+
+    if(strcmp(argv[1], ES_CMD_HELP) == 0 || strcmp(argv[1], ES_CMD_SHORT_HELP) == 0)
     {
         ELECTRON_SCAFFOLDER_help();
-        return 0;
+        return EXIT_SUCCESS;
     }
     ELECTRON_SCAFFOLDER_obj *es_obj = ELECTRON_SCAFFOLDER_create(argv);
-    ELECTRON_SCAFFOLDER_clean(es_obj);
 
-    return 0;
+    if(es_obj->project_name && es_obj->args_length)
+    {
+
+    }
+    else
+    {
+        /* Build the project in project name dir */
+
+    }
+    
+    ELECTRON_SCAFFOLDER_clean(es_obj);
+    return EXIT_SUCCESS;
 }
