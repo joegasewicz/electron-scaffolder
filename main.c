aabef4538d4a041c2b91c4834826f498927ab94d
joegasewicz <joegasewicz@gmail.com>
2020-08-29 14:57:52 +0100

    init

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+// --help commands
+
+int main()
+{
+
+    // get path
+
+    // add e-scaffolder to .bash_profile
+
+    // get users install path
+
+    // get user args
+
+    // 
+    return 0;
+}
