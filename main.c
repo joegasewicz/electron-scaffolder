c4033a171fe1528f534435a05dbe7b1bf6046180
joegasewicz <joegasewicz@gmail.com>
2020-08-29 16:44:32 +0100

    hellp cmd

--- main.c
+++ main.c
@@ -1,21 +1,44 @@
 #include <stdio.h>
+#include <string.h>
 
-#define E_SCAFFOLDER_CMD e-scaff
+#define ELECTRON_SCAFFOLDER_HELP_CMD " --help"
 
 // --help commands
 
-int main()
+typedef struct
 {
+    char *relative_path;
+} ELECTRON_SCAFFOLDER_obj;
 
+ELECTRON_SCAFFOLDER_obj *ELECTRON_SCAFFOLDER_create(char *argv[])
+{
+    ELECTRON_SCAFFOLDER_obj *es_obj = malloc(sizeof(ELECTRON_SCAFFOLDER_obj));
+    es_obj->relative_path = argv[0];
+    return es_obj;
+}
+
+void ELECTRON_SCAFFOLDER_help(void)
+{
+    printf("Help:\n --help\n");
+}
+
+
+int main(int argc, char *argv[])
+{
+    if(strcmp(argv[1], ELECTRON_SCAFFOLDER_HELP_CMD))
+    {
+        ELECTRON_SCAFFOLDER_help();
+        return 0;
+    }
     // get path
+    ELECTRON_SCAFFOLDER_obj *es_obj = ELECTRON_SCAFFOLDER_create(argv);
 
     // add e-scaffolder to .bash_profile
 
     // get users install path
 
     // get user args
-
+    
     // 
-    printf("Works......!\n");
     return 0;
 }
