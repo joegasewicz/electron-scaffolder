7136daaa0d413b26c4279b99e925da8089dae90b
joegasewicz <joegasewicz@gmail.com>
2020-08-29 17:21:39 +0100

    init

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #define ELECTRON_SCAFFOLDER_CMD_HELP " --help"
@@ -25,12 +26,24 @@ void ELECTRON_SCAFFOLDER_clean(ELECTRON_SCAFFOLDER_obj *es_obj)
 
 void ELECTRON_SCAFFOLDER_help(void)
 {
-    printf("Help:\n --help\n");
+    printf("\033[0;35m");
+    printf("Usage: electron-scaffolder <project_name> [options]\n\n");
+    printf("Options:\n");
+    printf("\t-f, --flat\t\tbuild the project in the current directory\n");
+    printf("\t-h, --help\t\toutput usage information\n");
+    printf("\t  Only <project_name> is requried.\n");
+    printf("\033[0m");
 }
 
 
 int main(int argc, char *argv[])
 {
+    if(!argv[1])
+    {
+        printf("ERROR: Please specify the project directory\n");
+        printf("\033[1;34m\telectron-scaffold <project_name>\033[0m\n");
+        return 0;
+    }
     if(strcmp(argv[1], ELECTRON_SCAFFOLDER_CMD_HELP))
     {
         ELECTRON_SCAFFOLDER_help();
